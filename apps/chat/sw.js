d67180117ab793edd0c88b31887cc3bf6563872e
AruzanSensei <alzanadytia.j@gmail.com>
2026-03-08 10:13:48 +0700

    update

---
+++ apps/chat/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'zanxa-ai-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './styles.css',
+    './script.js',
+    './favicon.png',
+    './favicon.ico'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+});
